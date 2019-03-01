5efc86e8a58adfe1fda89c2077b5fd961ed3dc36
pjaselin <26674234+pjaselin@users.noreply.github.com>
2019-03-01 00:32:42 -0500

    new .wav audio files with correct imports in keivy_demo/py

--- audiofiles/split_audio.py
+++ audiofiles/split_audio.py
@@ -2,18 +2,19 @@
 from pydub import AudioSegment
 # import full audio files from archives.org
 sound = AudioSegment.from_mp3("/home/pi/VTGuard/audiofiles/fdr_full.mp3")
-
+sound.export("/home/pi/VTGuard/audiofiles/fdr_full.wav", format="wav")
 # obtain and save audio segments
 
 # interval for audio split (len() and slicing are in milliseconds)
 # split length is total time/number of spluts
-split_length = len(sound)/5
+num_segments = 5
+split_length = len(sound)/num_segments
 
 i = 0
-while (i * split_length) <= len(sound):
+while i < num_segments:
     sample = sound[(split_length * i):(split_length * (i + 1))]
-    save_filepath = "/home/pi/VTGuard/audiofiles/sample_" + str(i) + ".mp3"
-    sample.export(save_filepath, format="mp3")
+    save_filepath = "/home/pi/VTGuard/audiofiles/sample_" + str(i) + ".wav"
+    sample.export(save_filepath, format="wav")
     i += 1
 
 # Concatenation is just adding
