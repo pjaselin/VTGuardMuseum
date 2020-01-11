2650132cb252a447327f8775dcab6215dd93a6bd
Patrick Aselin <26674234+pjaselin@users.noreply.github.com>
2020-01-11 11:44:59 -0500

    eod

--- rpi_shutdown.py
+++ rpi_shutdown.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 # when this code is called, fully and immediately shutdown the RPi
 import subprocess
 subprocess.call("sudo shutdown now", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
